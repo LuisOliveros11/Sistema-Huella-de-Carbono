a607616b3b45c46375390716d36122847536ec79
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2025-10-13 18:15:51 -0700

    Diseño de pantalla de inicio finalizada

--- src/tabs/HomeScreen.js
+++ src/tabs/HomeScreen.js
@@ -1,12 +1,13 @@
-import { StyleSheet, View, Text, ScrollView, ActivityIndicator, Dimensions } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, ActivityIndicator, Dimensions, TouchableOpacity } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Feather } from '@expo/vector-icons';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
+
 const cardWidth = screenWidth - 32;
 
 const HomeScreen = () => {
@@ -26,11 +27,46 @@ const HomeScreen = () => {
     <SafeAreaProvider style={styles.container}>
         <Text style={styles.title}>Contesta el cuestionario</Text>
         <View style={[styles.card, {backgroundColor: "#96D55F"}]}>
-           <FontAwesome5 style={styles.cardLogo} name="file-contract" size={118} color="white" />
+            <View style={{marginTop: screenHeight * 0.01}}>
+              <View style={[styles.horizontalContainer, { justifyContent: 'space-between'}]}>
+                <FontAwesome5 style={{paddingHorizontal: '5%'}} name="file-contract" size={118} color="white" />
+                <FontAwesome style={{paddingHorizontal: '11.5%'}} name="tree" size={60} color="white" />
+              </View>
+              <Text style={styles.cardText}>Calcula tu impacto ambiental</Text>
+              <View style={[styles.horizontalContainer, { justifyContent: 'space-between'}]}>
+                <Text style={[styles.cardText, {fontWeight: 300, fontSize: 11}]}>Contesta las preguntas del formulario para conocer tu huella de carbono</Text>
+                <TouchableOpacity
+                  onPress={async () => {
+                    //navigation.navigate('UserStatistics');
+                  }}>
+                  <View style={[styles.cardButton, {paddingHorizontal: '16%'}]}>
+                    <Text style={[styles.cardButtonText, {color: "#52C94B"}]}>Iniciar</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
         </View>
+
         <Text style={styles.title}>Conoce tus estadísticas</Text>
         <View style={[styles.card, {backgroundColor: "#79A4FF"}]}>
-            <Ionicons style={styles.cardLogo} name="stats-chart" size={118} color="white" />
+          <View style={{marginTop: screenHeight * 0.02}}>
+            <View style={[styles.horizontalContainer, { justifyContent: 'space-between'}]}>
+              <Ionicons style={{paddingHorizontal: '5%'}} name="stats-chart" size={105} color="white" />
+              <FontAwesome style={{paddingHorizontal: '7.5%'}} name="line-chart" size={70} color="white" />
+            </View>
+            <Text style={[styles.cardText, {width: screenWidth * .60}]}>Tus sugerencias personalizadas</Text>
+            <View style={[styles.horizontalContainer, { justifyContent: 'space-between'}]}>
+              <Text style={[styles.cardText, {fontWeight: 300, fontSize: 11}]}>Consulta tu nivel de huella de carbono y conoce en qué debes mejorar</Text>
+              <TouchableOpacity
+                onPress={async () => {
+                  //navigation.navigate('UserStatistics');
+                }}>
+                <View style={[styles.cardButton, {paddingHorizontal: '16%'}]}>
+                  <Text style={[styles.cardButtonText, {fontSize: 11}]}>Consultar</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
         
     </SafeAreaProvider>
@@ -59,13 +95,37 @@ const styles = StyleSheet.create({
   card: {
     backgroundColor: "blue",
     width: cardWidth,
-    height: 200,
+    height: 215,
     borderRadius: 18,
     marginBottom: 30
   },
   cardLogo: {
     marginTop: 12,
     marginLeft: 14
+  },
+  cardText: {
+    fontSize: 13,
+    fontWeight: 900,
+    color: "#fff",
+    marginTop: 10,
+    marginLeft: 14,
+    width: screenWidth * 0.50,
+  },
+  horizontalContainer: {
+    flexDirection: 'row',     
+    alignItems: 'center',      
+  },
+  cardButton: {
+    height: 35,
+    width: 90,
+    backgroundColor: '#fff',
+    borderRadius: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardButtonText: {
+    fontWeight: 600,
+    color: "#79A4FF",
   },
 
 
